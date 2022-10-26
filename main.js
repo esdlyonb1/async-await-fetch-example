6f4d90f5b7b156a7f6e7826939194a93114e1e8b
esdb1 <pierre.lacognata@mail-esd.com>
2022-10-26 10:22:59 +0200

    other commit

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
-fetch('https://api.chucknorris.io/jokes/random')
-    .then(uneReponse=>uneReponse.json())
-    .then(trucDésérialisé=>{
 
-    console.log(trucDésérialisé)
+
+function createJokeElement(joke){
+    let template =
+        `        
+        <li class="list-group-item">${joke}</li>
+        `
+
+    document.querySelector("#jokesList").innerHTML+=template
+}
+
+
+
+
+async function getAJoke(){
+
+    let reponse = await fetch('https://api.chucknorris.io/jokes/random')
+
+    let donnees = await reponse.json()
+
+    return donnees
+
+}
+
+document.querySelector("#add").addEventListener('click',()=>{
+    getAJoke().then(blague=>{
+        createJokeElement(blague.value)
     })
+})
+
+
+
+
