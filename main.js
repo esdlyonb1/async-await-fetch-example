e5708f348c9d3faf5e1af1733ff61eb7c741416c
esdb1 <pierre.lacognata@mail-esd.com>
2022-10-26 11:56:58 +0200

    other commit

--- main.js
+++ main.js
@@ -1,14 +1,64 @@
 
+function createCategoryButton(category){
+    let template =
+        `
+        <button id="${category}" class="btn btn-success col-3">${category}</button>
+        `
+
+    document.querySelector('#buttons').innerHTML+=template
+
+
+
+
+}
+
+getCategories().then(categories=>{
+    categories.forEach(category=>{
+        createCategoryButton(category)
+    })
+
+    let buttons = document.querySelectorAll('.btn-success')
+    buttons.forEach(button=>{
+        button.addEventListener('click',()=>{
+            getAJoke().then(joke=>{
+                createJokeElement(joke.value)
+
+                let delButtons = document.querySelectorAll('.suppr')
+                delButtons.forEach(delButton=>{
+                    delButton.addEventListener('click', ()=>{
+                        delButton.parentElement.remove()
+                    })
+                })
+
+            })
+
+        } )
+    })
+})
+
 
 function createJokeElement(joke){
     let template =
         `        
-        <li class="list-group-item">${joke}</li>
+        <li class="list-group-item">
+
+            ${joke}
+            
+            <button class="suppr btn btn-danger">
+                <i class="bi bi-trash"></i>
+            </button>
+        
+        </li>
         `
 
     document.querySelector("#jokesList").innerHTML+=template
 }
 
+async function getCategories(){
+    let reponse = await fetch('https://api.chucknorris.io/jokes/categories')
+    let donnees = await reponse.json()
+    return donnees
+}
 
 
 
@@ -26,6 +76,7 @@ document.querySelector("#add").addEventListener('click',()=>{
     getAJoke().then(blague=>{
         createJokeElement(blague.value)
     })
+
 })
 
 
